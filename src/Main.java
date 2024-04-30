e44ddc29454a35fdc0d1023bd9ec43ae4cee25e5
aleksandr.b <ser.sash@gmail.com>
2024-04-30 17:07:22 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BonusMilesService service = new BonusMilesService();
+        service.calculate(10_000);
+        int price = service.calculate(10_000) ;
+        System.out.println(price);
+
+        System.out.println();
+        System.out.println("15_000");
+        System.out.println("Ваш бонус: " +service.calculate(15_000));
+
+        System.out.println();
+        System.out.println("-20_000");
+        System.out.println("Ваш бонус: " +service.calculate(-20_000));
+
+        System.out.println();
+        System.out.println("2_000");
+        System.out.println("Ваш бонус: " +service.calculate(2_000));
+
+
+
+
+    }
+}
