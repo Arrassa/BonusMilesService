62f5ed8ddc44904435f69fb4957cced6b728473b
aleksandr.b <ser.sash@gmail.com>
2024-05-03 18:10:22 +0500

    chench commit

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,20 @@ public class Main {
 
         BonusMilesService service = new BonusMilesService();
         service.calculate(10_000);
-        int price = service.calculate(10_000) ;
+        int price = service.calculate(10_000);
         System.out.println(price);
 
         System.out.println();
         System.out.println("15_000");
-        System.out.println("Ваш бонус: " +service.calculate(15_000));
+        System.out.println("Ваш бонус: " + service.calculate(15_000));
 
         System.out.println();
         System.out.println("-20_000");
-        System.out.println("Ваш бонус: " +service.calculate(-20_000));
+        System.out.println("Ваш бонус: " + service.calculate(-20_000));
 
         System.out.println();
         System.out.println("2_000");
-        System.out.println("Ваш бонус: " +service.calculate(2_000));
-
-
+        System.out.println("Ваш бонус: " + service.calculate(2_000));
 
 
     }
